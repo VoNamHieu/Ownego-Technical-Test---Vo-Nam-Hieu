bfae78054d8c46a486918481eeba557ebe011196
hieuvo <103159852+VoNamHieu@users.noreply.github.com>
2024-06-06 18:05:37 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,43 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+var circle = document.querySelector('.logo');
+
+circle.style.backgroundColor = getRandomColor();
+
+var clickCount = 0;
+
+circle.addEventListener('click', function() {
+    clickCount++;
+
+    var currentColor = circle.style.backgroundColor;
+
+    if (clickCount < 9) {
+
+        var rgb = currentColor.substring(4, currentColor.length - 1).split(',').map(function(value) {
+            return parseInt(value);
+        });
+
+
+        rgb[0] -= 20;
+        rgb[1] -= 20;
+        rgb[2] -= 20;
+
+        rgb = rgb.map(function(value) {
+            return Math.max(0, value);
+        });
+
+        var newColor = 'rgb(' + rgb.join(',') + ')';
+
+        circle.style.backgroundColor = newColor;
+    } else {
+
+        circle.style.backgroundColor = '#000000';
+    }
+});
